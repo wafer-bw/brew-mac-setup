864d7742c8610ab5cac33225e83c40564fa2a8fa
wafer-bw <ben.jz.wakeford@gmail.com>
2020-09-16 16:26:10 -0400

    add output for golang path setup

--- setup.sh
+++ setup.sh
@@ -74,6 +74,7 @@ for i in ${npmGlobals[@]}; do
 done
 
 # Go paths
+echo "setting up golang"
 echo 'export GOPATH="${HOME}/.go"' >> ~/.zshrc-ext
 echo 'export GOROOT="$(brew --prefix golang)/libexec"' >> ~/.zshrc-ext
 echo 'export PATH="$PATH:${GOPATH}/bin:${GOROOT}/bin"' >> ~/.zshrc-ext
